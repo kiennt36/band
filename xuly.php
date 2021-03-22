b9a87a75a79d920a768e9fcb2dd92f1a571e9667
kiennt36 <kientrungngo2k1@gmail.com>
2021-03-22 18:12:19 +0700

    the first commit

---
+++ xuly.php
@@ -0,0 +1,10 @@
+<?php
+	$name = $_GET['username'];
+	$email = $_GET['email'];
+	$messenger = $_GET['messenger'];
+
+	echo "THONG TIN BAN VUA NHAP LA:<br>";
+	echo "Username: ". $name. "<br>";
+	echo "email: ". $email. "<br>";
+	echo "messenger: ". $messenger. "<br>";
+?>
